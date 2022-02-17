7f3f60ae070aa6446348a8b0da7b4737f35daf0b
SashaGitHub01 <sashab4b1n@yandex.ru>
2022-02-17 11:01:30 +0300

    fix router

--- src/components/AppRouter/index.jsx
+++ src/components/AppRouter/index.jsx
@@ -1,14 +1,14 @@
 import React, { Suspense, lazy } from 'react'
 import { Route, Routes, } from 'react-router-dom'
 import Loader from '../Loader'
-import Home from '../../pages/Home'
 
-const Login = lazy(() => import('../../pages/Login'))
-const Profile = lazy(() => import('../../pages/Profile'))
-const Register = lazy(() => import('../../pages/Register'))
-const Post = lazy(() => import('../../pages/Post'))
-const Password = lazy(() => import('../../pages/Password'))
-const NewPassword = lazy(() => import('../../pages/NewPassword'))
+import Home from '../../pages/Home'
+import Login from '../../pages/Login'
+import Profile from '../../pages/Profile'
+import Post from '../../pages/Post'
+import Register from '../../pages/Register'
+import Password from '../../pages/Password'
+import NewPassword from '../../pages/NewPassword'
 
 const AppRouter = () => {
    return (
